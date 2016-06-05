33ededad47c22bea7ca2cefa56ff47922af543f7
Sean Clayton 🎷🐛 <sean@seanclayton.me>
2016-06-05 10:50:51 -0400

    Add accessibility checking

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { install } from 'offline-plugin/runtime'
-import { whyDidYouUpdate } from 'why-did-you-update'
 import configureStore from './configureStore'
 import Root from 'containers/Root'
 import 'styles/styles.global.css'
 
 install()
 
-if (!__PROD__) {
+if (__DEV__) {
+  const whyDidYouUpdate = require('why-did-you-update').whyDidYouUpdate
   whyDidYouUpdate(React, {
     exclude: /^Connect/
   })
