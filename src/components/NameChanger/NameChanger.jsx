ba9293897cf15f5a6d8cf80e5e5a61bdc8e189e3
Sean Clayton 🎷🐢 <sean@seanclayton.me>
2016-06-05 10:50:51 -0400

    Add accessibility checking

--- src/components/NameChanger/NameChanger.jsx
+++ src/components/NameChanger/NameChanger.jsx
@@ -1,10 +1,20 @@
 import React, { PropTypes } from 'react'
 
-const NameChanger = ({ name, updateName }) =>
-  <div>
-    <h1>Hello {name}!</h1>
-    <input type="text" value={name} placeholder={name} onChange={(e) => updateName(e.target.value)} />
-  </div>
+const NameChanger = ({ name, updateName }) => {
+  const handleChange = (value) => updateName(value)
+
+  return (
+    <div>
+      <h1>Hello {name}!</h1>
+      <input
+        type="text"
+        value={name}
+        placeholder={name}
+        onChange={(e) => handleChange(e.target.value)}
+        onBlur={(e) => handleChange(e.target.value)} />
+    </div>
+  )
+}
 
 NameChanger.propTypes = {
   name: PropTypes.string,
