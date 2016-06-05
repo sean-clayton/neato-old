7ac172596d6db7dec744bb31eaf64728800e6fa8
Sean Clayton 🎷🐢 <sean@seanclayton.me>
2016-06-04 22:11:34 -0400

    Fix routing issues with client routing

--- scripts/server.js
+++ scripts/server.js
@@ -2,30 +2,35 @@ require('babel-register')
 
 const path = require('path')
 const express = require('express')
+const historyApiFallback = require('connect-history-api-fallback')
 const webpack = require('webpack')
 const config = require('../build/webpack.config')
 
 const app = express()
 const compiler = webpack(config)
 
+app.use(historyApiFallback({
+  verbose: false
+}))
+
 app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
+  noInfo: false,
   publicPath: config.output.publicPath,
+  contentBase: path.join(__dirname, '../src'),
   stats: {
     color: true
+  },
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: true
   }
 }))
 
 app.use(require('webpack-hot-middleware')(compiler))
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../src/index.html'))
-})
-
 app.listen(3000, 'localhost', (err) => {
   if (err) {
     console.log(err)
-    return
   }
 
   console.log('Listening at http://localhost:3000')
