c3ca088e2c0ed312dc903cdd6076851d94a14b19
Sean Clayton 🎷🐛 <sean@seanclayton.me>
2016-06-16 16:44:35 -0400

    added storybook folder

---
+++ storybook/config.js
@@ -0,0 +1,12 @@
+/* Mock config file:
+
+import { configure } from '@kadira/storybook';
+import 'todomvc-app-css/index.css'
+
+function loadStories() {
+  require('../components/stories/');
+}
+
+configure(loadStories, module);
+
+*/
