3a3443ccb62da0978f5899d587279211eda527bc
Sean Clayton 🎷🐛 <sean@seanclayton.me>
2016-07-03 11:25:31 -0400

    Add new TypeScript deps
    Also delete src
    Related: #7 #9

---
+++ definitions/index.d.ts
@@ -0,0 +1,7 @@
+export interface INeato {
+  run(): any
+}
+
+export interface INeatoConfig {
+
+}
--- src/actions/name.js
+++
@@ -1,8 +0,0 @@
-// Constants
-export const UPDATE_NAME = 'UPDATE_NAME'
-
-// Actions
-export const updateName = name => ({
-  type: UPDATE_NAME,
-  payload: name
-})
