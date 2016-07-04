746b273ec0fd3cf772f96271379ca24ab0de9356
Sean Clayton 🎷🐛 <sean@seanclayton.me>
2016-07-04 17:04:30 -0400

    Use TypeScript for Runner

---
+++ src/runner/index.ts
@@ -0,0 +1,19 @@
+import developmentServer from './development-server'
+import build from './build'
+import test from './test'
+import install from './install'
+import lint from './lint'
+import actions from '../actions'
+
+const runner = (neatoOptions) => {
+  switch (neatoOptions.action) {
+    case value:
+      
+      break;
+  
+    default:
+      break;
+  }
+}
+
+export default runner
