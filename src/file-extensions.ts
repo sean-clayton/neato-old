7875371741495e9ceca0d0c5dcf56d9f8e9315be
Sean Clayton 🎷🐛 <sean@seanclayton.me>
2016-07-08 16:36:45 -0400

    feat(file-extensions): Add file extensions

---
+++ src/file-extensions.ts
@@ -0,0 +1,13 @@
+export default {
+  list: {
+    JAVASCRIPT: ['.js', '.jsx'],
+    TYPESCRIPT: ['.ts', '.tsx']
+  },
+  test: {
+    CSS: /\.css$/,
+    IMAGE: /\.(png|jpg|jpeg|gif|svg)$/,
+    JAVASCRIPT: /\.jsx?$/,
+    TYPESCRIPT: /\.tsx?$/,
+    JSON: /\.json/
+  }
+}
