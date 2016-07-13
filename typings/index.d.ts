c0e456a928648b5d581a342078a51a3e470d9e9c
Sean Clayton 🎷🐛 <sean@seanclayton.me>
2016-07-13 08:34:49 -0400

    chore(project): Remove TypeScript config

--- typings/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="globals/commander/index.d.ts" />
-/// <reference path="globals/fs-extra/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/source-map/index.d.ts" />
-/// <reference path="globals/uglify-js/index.d.ts" />
-/// <reference path="globals/webpack/index.d.ts" />
