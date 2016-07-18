7986acbb92e804155f2654911736c1346824d64c
Sean Clayton 🎷🐛 <sean@seanclayton.me>
2016-07-18 15:53:47 -0400

    feat(webpack): Add accessible env globals

--- src/webpack/presets/define-node-env.js
+++ src/webpack/presets/define-node-env.js
@@ -1,12 +1,17 @@
 import { DefinePlugin } from 'webpack'
 
+const env = process.env.NODE_ENV || 'development'
+
 export default {
   name: 'define-node-env',
   configure() {
     return {
       plugins: [
         new DefinePlugin({
-          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+          'process.env.NODE_ENV': JSON.stringify(env),
+          '__DEV__': env === 'development',
+          '__PROD__': env === 'production',
+          '__TEST__': env === 'test'
         })
       ]
     }
