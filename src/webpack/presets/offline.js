70fb2b7871b6b576c998a7069f87c4766809c374
Sean Clayton 🎷🐛 <sean@seanclayton.me>
2016-07-31 11:12:49 -0400

    fix(offline): Make offline true by default

--- src/webpack/presets/offline.js
+++ src/webpack/presets/offline.js
@@ -2,7 +2,7 @@ import OfflinePlugin from 'offline-plugin'
 
 export default {
   name: 'offline',
-  configure({ offline, offlineOptions }) {
+  configure({ offline = true, offlineOptions = {} }) {
     // offlineOptions is a valid offline-plugin config object
     // See: https://www.npmjs.com/package/offline-plugin
     return offline ? { plugins: [new OfflinePlugin(offlineOptions)] } : {}
