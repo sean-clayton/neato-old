7cbcf9ac677262a2af23009d8c0e560bacd6b510
Sean Clayton 🎷🐛 <sean@seanclayton.me>
2016-08-04 11:28:23 -0400

    fix(webpack): Add include path to images

--- src/webpack/loaders/image.js
+++ src/webpack/loaders/image.js
@@ -1,13 +1,17 @@
+import path from 'path'
 import fileExtensions from '../../file-extensions'
 
 export default {
   name: 'image',
-  configure() {
+  configure({ projectPath }) {
     return {
       module: {
         loaders: [
           {
             test: fileExtensions.test.IMAGE,
+            include: [
+              path.resolve(projectPath, 'src')
+            ],
             loader: 'url-loader?limit=8192&name=[name]-[chunkhash].[ext]'
           }
         ]
